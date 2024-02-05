c1b352a8be95a579e07990a7999330c72c22dd8d
neilsen zulueta <neilzulueta@gmail.com>
2024-02-04 22:49:53 -0800

    updated thoughtroutes.js

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -5,10 +5,16 @@ router.route('/')
     .post((req, res) => res.send('this is POST `/api/thoughts`'))
 
 router.route('/:thoughtId')
-    .get((req, res) => res.send('this is GET `/api/thoughts/${req.params.thoughtId}`'))
-    .put((req, res) => res.send('this is PUT `/api/thoughts/${req.params.thoughtId}`'))
-    .delete((req, res) => res.send('this is DELETE `/api/thoughts/${req.params.thoughtId}`'))
+    .get((req, res) => res.send(`this is GET /api/thoughts/${req.params.thoughtId}`))
+    .put((req, res) => res.send(`this is PUT /api/thoughts/${req.params.thoughtId}`))
+    .delete((req, res) => res.send(`this is DELETE /api/thoughts/${req.params.thoughtId}`))
 
+router.route('/:thoughtId/reactions/')
+    .post((req, res) => res.send(
+        `this is POST '/api/thoughts/${req.params.thoughtId}/reactions'`))
 
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete((req, res) => res.send(
+    `this is POST '/api/thoughts/${req.params.thoughtId}/${req.params.reactionId}'`))
 
 module.exports = router
